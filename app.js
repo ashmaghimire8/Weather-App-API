8d0143689ac9a2257c6f6368f5e799f6d9c160e9
sugafree8 <ashmaghimire8@gmail.com>
2022-12-24 18:49:42 +0545

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));   
+
 app.get("/",function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid=b4d526551a06e24f3ab13bce37b959fd&units=metric"
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+    const query = req.body.cityName;
+    const apiKey = "";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+    
     https.get(url,function(response){
         console.log(response.statusCode);
 
@@ -14,16 +25,15 @@ app.get("/",function(req,res){
             const weatherDesc = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL ="http://openweathermap.org/img/wn/"+icon+"@2x.png"
-            res.write("<h1>The temperature in Kathmandu is "+temp+"degree celcius</h1>");
+            res.write("<h1>The temperature in "+query+" is "+temp+"degree celcius</h1>");
             res.write("The weather condition is " + weatherDesc);
             res.write("<img src="+imageURL+">");
             res.send();
         })
     })
-})
-
-
+});
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000")
 })
+
